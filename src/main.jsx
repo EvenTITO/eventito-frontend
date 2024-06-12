c1acb72584308f1854fb09fbb0b5a02476c6a12c
Fernando Sinisi <fernando.sinisi@despegar.com>
2024-06-11 23:23:40 -0300

    FixReload - added firebase onAuthStateChanged
    Signed-off-by: Fernando Sinisi <fernando.sinisi@despegar.com>

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 import { persistor, store } from '@/services/state/store.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -11,9 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PersistGate persistor={persistor}>
       <Provider store={store}>
-        <BrowserRouter>
           <App />
-        </BrowserRouter>
       </Provider>
     </PersistGate>
   </React.StrictMode>,
