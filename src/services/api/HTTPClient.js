be2435f62d76921f4bc782ef426e393103e30d23
mateocapon <mcapon@fi.uba.ar>
2024-08-31 15:58:36 -0400

    add http client + get event

---
+++ src/services/api/HTTPClient.js
@@ -0,0 +1,31 @@
+import { getAuthUser } from "@/services/firebase/firebaseServices.js";
+import axios from "axios";
+
+
+export class HTTPClient {
+    axiosInstance;
+
+    constructor(baseURL) {
+        this.axiosInstance = axios.create({baseURL: baseURL});
+    }
+
+    createHeaders() {
+        // TODO: the token should update itself, I think this is asyn.
+        return { 
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${getAuthUser().stsTokenManager.accessToken}`
+            }
+        };
+    }
+
+    async post(url, body) {
+        headers = createHeaders();
+        return await this.axiosInstance.post(url, body, headers);
+    }
+
+    async get(url) {
+        headers = createHeaders();
+        return await this.axiosInstance.get(url, headers);
+    }
+}
