4510f278f9eb0c79945517e39bef61c353456270
gsabatino9 <gsabatino@fi.uba.ar>
2024-08-31 17:14:02 -0300

    lint & update todos

--- src/services/api/events/general/queries.js
+++ src/services/api/events/general/queries.js
@@ -1,16 +1,15 @@
 import { EVENTS_URL } from "@/lib/Constants";
-import { HTTPClient } from "@/services/api/HTTPClient"
+import { HTTPClient } from "@/services/api/HTTPClient";
 
 const httpClient = new HTTPClient(EVENTS_URL);
 
-
 export const apiGetEventById = async (eventId) => {
-    try {
-        const url = `/${eventId}/public`;
-        const res = await httpClient.get(url);
-        return res.data;
-    } catch (err) {
-        console.log("Error en obtener el evento", err);
-        throw err;
-    }
-}
+  try {
+    const url = `/${eventId}/public`;
+    const res = await httpClient.get(url);
+    return res.data;
+  } catch (err) {
+    console.log("Error en obtener el evento", err);
+    throw err;
+  }
+};
