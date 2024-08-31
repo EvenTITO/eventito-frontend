f3b1c9d7e9fcb2ebf087a616ad93770bf408b8c8
gsabatino9 <gsabatino@fi.uba.ar>
2024-08-31 18:17:38 -0300

    get my events

--- src/services/api/events/general/queries.js
+++ src/services/api/events/general/queries.js
@@ -1,11 +1,14 @@
 import { EVENTS_URL } from "@/lib/Constants";
 import { HTTPClient } from "@/services/api/HTTPClient";
+import { z } from "zod";
 
 const httpClient = new HTTPClient(EVENTS_URL);
 
+const uuidSchema = z.string().uuid();
+
 export const apiGetEventById = async (eventId) => {
   try {
-    return (await httpClient.get(`/${eventId}/public`)).data;
+    return (await httpClient.get(`/${uuidSchema.parse(eventId)}/public`)).data;
   } catch (err) {
     throw err;
   }
