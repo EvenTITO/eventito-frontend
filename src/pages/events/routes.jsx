ea135fb58304867bb01a8e177e6d3ac80fbfa8e8
gsabatino9 <gsabatino@fi.uba.ar>
2024-09-01 20:31:11 -0300

    feat: better event view page

--- src/pages/events/routes.jsx
+++ src/pages/events/routes.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import EventViewPage from "./[id]/view/page";
-import LayoutEvent from "./layout";
+import LayoutEvents from "./layout";
 
 export default function RoutesEvent() {
   return (
     <Routes>
-      <Route path="/events" element={<LayoutEvent />}>
+      <Route path="/events" element={<LayoutEvents />}>
         <Route path="/events/:id/view" element={<EventViewPage />} />
         <Route path="/events/:id/view/general" element={<EventViewPage />} />
       </Route>
