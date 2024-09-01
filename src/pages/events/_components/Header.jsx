d04bd4eab3678ff7ce5631215f87f1705cf900c4
gsabatino9 <gsabatino@fi.uba.ar>
2024-09-01 20:44:45 -0300

    feat: go back! (to the future?)

--- src/pages/events/_components/Header.jsx
+++ src/pages/events/_components/Header.jsx
@@ -2,11 +2,14 @@ import { Button } from "@/components/ui/button";
 import { List, User } from "lucide-react";
 
 export default function Header({ headerTitle, toggleSidebar }) {
-  const title = headerTitle.length < 50 ? headerTitle : `${headerTitle.slice(0,50)}...`;
+  const title =
+    headerTitle.length < 50 ? headerTitle : `${headerTitle.slice(0, 50)}...`;
 
   return (
     <header className="h-16 bg-[#121827] border-b flex items-center justify-between px-4 fixed top-0 left-0 right-0 z-50 text-white">
       <div className="flex items-center">
+        <GoBack />
+        <div className={`h-6 border-l border-gray-500 mx-4`}></div>
         <Button
           variant="outline"
           size="icon"
@@ -26,3 +29,22 @@ export default function Header({ headerTitle, toggleSidebar }) {
   );
 }
 
+import { Link } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
+
+function GoBack() {
+	return (
+		<Link
+			to={"/home"}
+			className={`flex items-center gap-2 text-tabNotSelected hover:text-white`}
+			style={{ fontWeight: 'normal' }}
+		>
+			<ArrowLeft className="h-4 w-4" />
+			<span
+				style={{ fontSize: '0.82rem' }}
+			>
+				volver
+			</span>
+		</Link>
+	);
+}
