6527e053a749d5083e1262ca9ff5229b45ae6874
gsabatino9 <gsabatino@fi.uba.ar>
2024-09-08 17:03:32 -0300

    feat: add first try of sidebar with roles

--- src/pages/(events-manage)/events/layout.jsx
+++ src/pages/(events-manage)/events/layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet, useParams } from "react-router-dom";
 import Header from "../_components/Header";
+import Sidebar from "./_components/Sidebar";
 import { useDispatch, useSelector } from "react-redux";
 import { getEvent } from "@/services/api/events/general/hooks";
 import { loadEvent } from "@/state/events/eventSlice";
@@ -7,10 +8,10 @@ import FetchStatus from "@/components/FetchStatus";
 
 export default function LayoutEvents() {
   const { id: eventIdParams } = useParams();
-  const { eventId, eventTitle } = useSelector((state) => state.event);
+  const { eventId, eventTitle, roles } = useSelector((state) => state.event);
   const dispatch = useDispatch();
 
-  const layoutComponent = <Layout eventTitle={eventTitle} />;
+  const layoutComponent = <Layout eventTitle={eventTitle} roles={roles} />;
   if (eventId && eventId === eventIdParams) {
     return layoutComponent;
   } else {
@@ -40,16 +41,25 @@ export default function LayoutEvents() {
   }
 }
 
-function Layout({ eventTitle }) {
+function Layout({ eventTitle, roles }) {
   return (
     <div className="flex flex-col h-screen bg-background">
       <Header toggleSidebar={false} headerTitle={eventTitle} />
 
-      <div className="pt-12">
-        <main>
-          <Outlet />
-        </main>
-      </div>
+      {roles.length > 0 ? (
+        <div className="flex flex-1 pt-16">
+          <Sidebar isSidebarOpen={true} />
+          <main className="flex-1 p-4 md:ml-64 pt-4 overflow-auto">
+            <Outlet />
+          </main>
+        </div>
+      ) : (
+        <div className="pt-12">
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      )}
     </div>
   );
 }
