8043b93e25d6babb1ec263427ed13bff69c54bfa
gsabatino9 <gsabatino@fi.uba.ar>
2024-09-15 14:00:54 -0300

    fix: fixed path if it is invalid

--- src/lib/navigation.js
+++ src/lib/navigation.js
@@ -1,14 +1,21 @@
 import { useNavigate, useLocation } from "react-router-dom";
 
 class Navigator {
-  constructor(navigate, location) {
+  constructor(navigate, location, startRoute = null) {
     this.navigate = navigate;
     this.location = location;
+    this.startRoute = startRoute;
   }
 
   foward(path) {
-    const relativePath = `${this.location.pathname}/${path}`;
-    this.navigate(relativePath);
+    const fixedPath = path[0] === "/" ? path.slice(1) : path;
+    const relativePath = `${this.location.pathname}/${fixedPath}`;
+    this.to(relativePath);
+  }
+
+  back() {
+    const path = this.location.pathname.split(this.startRoute)[0];
+    this.to(path);
   }
 
   to(path) {
@@ -16,9 +23,9 @@ class Navigator {
   }
 }
 
-export const useNavigator = () => {
+export const useNavigator = (startRoute = "/home") => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  return new Navigator(navigate, location);
+  return new Navigator(navigate, location, startRoute);
 };
