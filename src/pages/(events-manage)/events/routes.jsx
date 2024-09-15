84bed6610b470a7d5854d7d4ab957c66211b8cdd
gsabatino9 <gsabatino@fi.uba.ar>
2024-09-15 17:53:17 -0300

    refactor: chair page

--- src/pages/(events-manage)/events/routes.jsx
+++ src/pages/(events-manage)/events/routes.jsx
@@ -5,6 +5,7 @@ import ChairPage from "./[id]/roles/chair";
 import ReviewerPage from "./[id]/roles/reviewer";
 import AuthorPage from "./[id]/roles/author";
 import AssignmentPage from "./[id]/roles/reviewer/assigment";
+import ChairWorkPage from "./[id]/roles/chair/work";
 
 export default function RoutesEvents() {
   return (
@@ -13,8 +14,15 @@ export default function RoutesEvents() {
         <Route path="/events2/:id/view" element={<EventViewPage />} />
         <Route path="/events2/:id/view/general" element={<EventViewPage />} />
         <Route path="/events2/:id/roles/chair" element={<ChairPage />} />
+        <Route
+          path="/events2/:id/roles/chair/works/:workId"
+          element={<ChairWorkPage />}
+        />
         <Route path="/events2/:id/roles/reviewer" element={<ReviewerPage />} />
-        <Route path="/events2/:id/roles/reviewer/assignments/:assignmentId" element={<AssignmentPage />} />
+        <Route
+          path="/events2/:id/roles/reviewer/assignments/:assignmentId"
+          element={<AssignmentPage />}
+        />
         <Route path="/events2/:id/roles/author" element={<AuthorPage />} />
       </Route>
     </Routes>
