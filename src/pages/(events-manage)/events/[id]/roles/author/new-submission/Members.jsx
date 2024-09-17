de487d40ea7dc5fb9fe50c6de7f0cf66b48887ca
gsabatino9 <gsabatino@fi.uba.ar>
2024-09-16 23:25:11 -0300

    refactor: author page

--- src/pages/(events-manage)/events/[id]/roles/author/new-submission/Members.jsx
+++ src/pages/(events-manage)/events/[id]/roles/author/new-submission/Members.jsx
@@ -1,3 +1,24 @@
+import { useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import AddAuthorDialog from "./_components/AddAuthorDialog";
+import AuthorsTable from "./_components/AuthorsTable";
+
 export default function Members() {
-  return <div>members</div>
+  const [authors, setAuthors] = useState([]);
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className={"flex items-center justify-between"}>
+          <span>Autores del trabajo</span>
+          <AddAuthorDialog
+            newAuthor={(author) => setAuthors([...authors, author])}
+          />
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <AuthorsTable authors={authors} />
+      </CardContent>
+    </Card>
+  );
 }
