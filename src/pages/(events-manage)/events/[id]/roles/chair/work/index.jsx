fbb8e0ee3941ad67e9db178831f20745c77331f9
gsabatino9 <gsabatino@fi.uba.ar>
2024-09-22 18:44:01 -0300

    nicer loading

--- src/pages/(events-manage)/events/[id]/roles/chair/work/index.jsx
+++ src/pages/(events-manage)/events/[id]/roles/chair/work/index.jsx
@@ -1,37 +1,22 @@
 import FetchStatus from "@/components/FetchStatus";
 import Page from "./page";
+import { useGetReviewsForAssignment } from "@/hooks/events/chairHooks";
 import {
-  useGetReviewsForAssignment,
-} from "@/hooks/events/chairHooks";
-import { useGetWorkById, useGetWorkDownloadURL } from "@/hooks/events/worksHooks";
-import { useToast } from "@/hooks/use-toast";
+  useGetWorkById,
+  useGetWorkDownloadURL,
+} from "@/hooks/events/worksHooks";
 import { useEffect } from "react";
 
 export default function ChairWorkPage() {
   // TODO: cambiar esto para que sea global?
   const workInfo = useGetWorkById();
   const reviews = useGetReviewsForAssignment();
-  const {data: fileData, mutate: downloadWorkFile, isError, isPending, isSuccess, error} = useGetWorkDownloadURL();
-  const { toast } = useToast();
-
-  useEffect(() => {
-    if (isPending) {
-      console.log("loading...");
-    }
-    if (isError) {
-      toast({
-        variant: "destructiveOutline",
-        title: "Error al descargar trabajo",
-        description: "El autor no ha subido un archivo todavia",
-      });
-    }
-    if (isSuccess) {
-      window.open(fileData.download_url.download_url, "_blank");
-    }
-  }, [isPending, isError, isSuccess, toast]);
 
   const component = (
-    <Page selectedWork={workInfo.data} reviews={reviews.data} getFileData={downloadWorkFile} />
+    <Page
+      selectedWork={workInfo.data}
+      reviews={reviews.data}
+    />
   );
   return (
     <FetchStatus
