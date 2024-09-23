37ad9775d670722874f76f04bb4b0501ef9490a5
Lucas <lveron@prismamp.com>
2024-09-22 23:53:02 -0300

    fix import

--- src/pages/(events-manage)/events/[id]/roles/chair/work/index.jsx
+++ src/pages/(events-manage)/events/[id]/roles/chair/work/index.jsx
@@ -1,11 +1,11 @@
 import FetchStatus from "@/components/FetchStatus";
 import Page from "./page";
-import {useGetReviewsForAssignment} from "@/hooks/events/chairHooks";
+import {useGetReviewsForWork} from "@/hooks/events/chairHooks";
 import {useGetWorkById} from "@/hooks/events/worksHooks";
 
 export default function ChairWorkPage() {
     const workInfo = useGetWorkById();
-    const reviews = useGetReviewsForAssignment();
+    const reviews = useGetReviewsForWork();
 
     const component = (
         <Page selectedWork={workInfo.data} reviews={reviews.data}/>
