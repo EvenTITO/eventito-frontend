544bf1f2b9e62b9b9ad9b9818a77ec64d227384a
gsabatino9 <gsabatino@fi.uba.ar>
2024-09-26 20:22:06 -0300

    refactor: author page

--- src/pages/(events-manage)/events/[id]/roles/author/page.jsx
+++ src/pages/(events-manage)/events/[id]/roles/author/page.jsx
@@ -1,13 +1,9 @@
 import { useNavigator } from "@/lib/navigation";
 import ContainerPage from "@/pages/(events-manage)/_components/containerPage";
 import TitlePage from "@/pages/(events-manage)/_components/titlePage";
-
-import { Plus } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { useGetEvent } from "@/hooks/events/useEventState";
-import { dateIsValid, getDeadlineSubmissions } from "@/lib/dates";
 import WorksTable from "./_components/WorksTable";
 import TableContent from "@/components/TableContent";
+import NewSubmissionButton from "./_components/NewSubmissionButton";
 
 export default function Page({ works }) {
   const navigator = useNavigator();
@@ -27,25 +23,5 @@ export default function Page({ works }) {
         <WorksTable works={works} handleRowClick={handleRowClick} />
       </TableContent>
     </ContainerPage>
-  );
-}
-
-function NewSubmissionButton() {
-  const navigator = useNavigator();
-  const { data: eventData } = useGetEvent();
-  const deadlineSubmissions = getDeadlineSubmissions(eventData)?.date;
-
-  function handleNewSubmission() {
-    navigator.foward("new-submission");
-  }
-
-  if (!dateIsValid(deadlineSubmissions)) {
-    return null;
-  }
-
-  return (
-    <Button onClick={handleNewSubmission}>
-      <Plus className="mr-2 h-4 w-4" /> Nueva entrega
-    </Button>
   );
 }
