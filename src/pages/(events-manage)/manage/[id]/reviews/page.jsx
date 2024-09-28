87e09f0509f66c5b9e2df95f8d1320da6a7f3104
gsabatino9 <gsabatino@fi.uba.ar>
2024-09-28 17:24:44 -0300

    reviews page init

--- src/pages/(events-manage)/manage/[id]/reviews/page.jsx
+++ src/pages/(events-manage)/manage/[id]/reviews/page.jsx
@@ -1,3 +1,11 @@
+import ContainerPage from "@/pages/(events-manage)/_components/containerPage";
+import TitlePage from "@/pages/(events-manage)/_components/titlePage";
+
 export default function Page({ questions }) {
-  return <div>Reviews</div>;
+  return (
+    <ContainerPage>
+      <TitlePage title={"Administración de formulario de revisión"} />
+      <div className="space-y-6 pt-6">Form de revisión</div>
+    </ContainerPage>
+  );
 }
