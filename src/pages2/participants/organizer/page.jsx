c549acfeea6847a8315e81bfe81aae1b0055c846
gsabatino9 <gsabatino@fi.uba.ar>
2024-09-28 17:29:05 -0300

    delete untrashed files#

--- src/pages2/participants/organizer/page.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, useParams } from "react-router-dom";
-
-export default function OrganizerDummyPage() {
-  const { id } = useParams();
-
-  return <Navigate to={`manage/${id}/general`} replace />;
-}
