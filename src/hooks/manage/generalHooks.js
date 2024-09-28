9d550d6423a115f40bab0c325f63d2c9c15183a1
gsabatino9 <gsabatino@fi.uba.ar>
2024-09-28 19:30:09 -0300

    remove dependencies unused

--- src/hooks/manage/generalHooks.js
+++ src/hooks/manage/generalHooks.js
@@ -1,9 +1,9 @@
-import {getEventId, wait} from "@/lib/utils";
-import {useMutation, useQueryClient} from "@tanstack/react-query";
-import {HTTPClient} from "@/services/api/HTTPClient.js";
-import {EVENTS_URL} from "@/lib/Constants.js";
-import {uploadFile} from "@/services/api/storage/queries.js";
-import {apiGetUploadEventImageUrl} from "@/services/api/events/general/queries.js";
+import { getEventId, wait } from "@/lib/utils";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { HTTPClient } from "@/services/api/HTTPClient.js";
+import { EVENTS_URL } from "@/lib/Constants.js";
+import { uploadFile } from "@/services/api/storage/queries.js";
+import { apiGetUploadEventImageUrl } from "@/services/api/events/general/queries.js";
 
 export function useEditEvent() {
   const eventId = getEventId();
@@ -11,13 +11,13 @@ export function useEditEvent() {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async ({eventData}) => {
+    mutationFn: async ({ eventData }) => {
       await wait(2);
       return null;
     },
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ["getEventById", {eventId}],
+        queryKey: ["getEventById", { eventId }],
       });
     },
   });
@@ -28,22 +28,27 @@ export function useUploadEventImage() {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async ({imageName, image}) => await uploadEventImage(eventId, imageName, image),
+    mutationFn: async ({ imageName, image }) =>
+      await uploadEventImage(eventId, imageName, image),
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ["getEventById", {eventId}],
+        queryKey: ["getEventById", { eventId }],
       });
     },
   });
 }
 
 async function uploadEventImage(eventId, imageName, image) {
-  console.log(imageName)
-  console.log(image)
+  console.log(imageName);
+  console.log(image);
   if (image) {
-    const httpClient = new HTTPClient(EVENTS_URL)
-    const uploadUrl = await apiGetUploadEventImageUrl(httpClient, eventId, imageName)
-    console.log(uploadUrl)
-    await uploadFile(uploadUrl, image)
+    const httpClient = new HTTPClient(EVENTS_URL);
+    const uploadUrl = await apiGetUploadEventImageUrl(
+      httpClient,
+      eventId,
+      imageName,
+    );
+    console.log(uploadUrl);
+    await uploadFile(uploadUrl, image);
   }
 }
