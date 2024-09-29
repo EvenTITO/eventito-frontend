93baa79678075f88f6791e1c4c7436dedced23bf
gsabatino9 <gsabatino@fi.uba.ar>
2024-09-29 16:48:29 -0300

    feat: tracks integration

--- src/pages/(events-manage)/manage/[id]/tracks/index.jsx
+++ src/pages/(events-manage)/manage/[id]/tracks/index.jsx
@@ -1,16 +1,51 @@
 import FetchStatus from "@/components/FetchStatus";
 import Page from "./page";
 import { useGetEventChairsByTracks } from "@/hooks/manage/tracksHooks";
+import { useGetEvent } from "@/hooks/events/useEventState";
 
 export default function TracksConfigPage() {
-  const { data: tracksByChair, isPending, error } = useGetEventChairsByTracks();
+  const eventData = useGetEvent();
+  const tracksSettled = eventData.data?.tracks;
+
+  const {
+    data: tracksByChair,
+    isPending,
+    error,
+  } = useGetEventChairsByTracks({
+    enabled: !!tracksSettled,
+  });
 
   if (tracksByChair) {
     console.log("tracks by chair", tracksByChair);
+    console.log("tracks", eventData.data.tracks);
   }
 
-  const component = <Page tracks={tracksByChair || []} />;
-  return (
-    <FetchStatus component={component} isPending={isPending} error={error} />
+  const component = (
+    <Page
+      tracks={unifyEventTracksWithChairs(
+        eventData.data.tracks,
+        tracksByChair || [],
+      )}
+    />
   );
+  return (
+    <FetchStatus
+      component={component}
+      isPending={isPending || eventData.isPending}
+      error={error || eventData.error}
+    />
+  );
+}
+
+function unifyEventTracksWithChairs(eventTracks, tracksByChair) {
+  eventTracks.forEach((trackName) => {
+    const trackExists = tracksByChair.some(
+      (track) => track.track === trackName,
+    );
+    if (!trackExists) {
+      tracksByChair.push({ track: trackName });
+    }
+  });
+
+  return tracksByChair;
 }
