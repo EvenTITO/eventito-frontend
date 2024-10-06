6c76bb4e5ee9122406b1b10eb39a96b4b1913861
gsabatino9 <gsabatino@fi.uba.ar>
2024-10-06 01:00:03 -0300

    container

--- src/pages/(events-manage)/_components/containerPage.jsx
+++ src/pages/(events-manage)/_components/containerPage.jsx
@@ -1,3 +1,3 @@
 export default function ContainerPage({ children }) {
-  return <div className="container mx-auto py-10">{children}</div>
+  return <div className="container mx-auto py-4">{children}</div>
 }
