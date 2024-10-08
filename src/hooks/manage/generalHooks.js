def336cc558ea6c51d1debdec256839a9a8fa8dc
Fernando Sinisi <fernando.sinisi@despegar.com>
2024-10-07 22:49:19 -0300

    event_images - add edit banner image
    Signed-off-by: Fernando Sinisi <fernando.sinisi@despegar.com>

--- src/hooks/manage/generalHooks.js
+++ src/hooks/manage/generalHooks.js
@@ -5,26 +5,41 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { uploadFile } from '@/services/api/storage/queries.js'
 import {
   apiGetUploadEventImageUrl,
-  apiUpdateGeneralEvent,
   apiUpdateEventStatus,
+  apiUpdateGeneralEvent,
 } from '@/services/api/events/general/queries'
+import { useToastMutation } from '@/hooks/use-toast-mutation.js'
 
 export function useEditEvent() {
   const eventId = getEventId()
 
   const queryClient = useQueryClient()
 
-  return useMutation({
-    mutationFn: async ({ eventData }) => {
+  return useToastMutation(
+    async ({ eventData }) => {
       const httpClient = new HTTPClient(EVENTS_URL)
       await apiUpdateGeneralEvent(httpClient, eventId, { ...eventData })
     },
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ['getEventById', { eventId }],
-      })
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ['getEventById', { eventId }],
+        })
+      },
     },
-  })
+    {
+      success: {
+        show: true,
+        title: 'Evento editado',
+        message: 'Se editó la información del evento de forma correcta',
+      },
+      error: {
+        title: 'Edición de evento fallida',
+        message:
+          'Ocurrió un error al editar el evento. Por favor intente más tarde',
+      },
+    }
+  )
 }
 
 export function useUploadEventImage() {
--- src/pages/(events-manage)/manage/[id]/_components/ImageHeader.jsx
+++ src/pages/(events-manage)/manage/[id]/_components/ImageHeader.jsx
@@ -1,13 +1,69 @@
-export default function ImageHeader({ image }) {
+import { useRef, useState } from 'react'
+
+export default function ImageHeader({
+  image,
+  isEditing,
+  newBannerFile,
+  setBannerFile,
+}) {
+  const [isHovered, setIsHovered] = useState(false)
+  const showImagePicker = useRef(false)
+
+  const handleMouseOver = () => {
+    setIsHovered(true)
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false)
+  }
+
+  const handleClick = () => {
+    showImagePicker.current.click()
+  }
+
+  const handleFileChange = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      setBannerFile(e.target.files[0])
+    }
+  }
+
   return (
-    <div className="w-full h-[300px] mb-8 rounded-lg overflow-hidden">
-      <img
-        src={image.url}
-        onError={(e) => {
-          e.target.src =
-            'https://cphfcrflaa.cloudimg.io/_bcuimages/academic-conference-primary-132249422941807450.jpg'
-        }}
-        className="w-full h-full object-cover"
+    <div onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+      <button
+        onClick={handleClick}
+        disabled={!isEditing}
+        className="w-full h-[300px] mb-8 rounded-lg overflow-hidden"
+      >
+        {isEditing ? (
+          <img
+            src={newBannerFile ? URL.createObjectURL(newBannerFile) : image.url}
+            onError={(e) => {
+              e.target.src =
+                'https://cphfcrflaa.cloudimg.io/_bcuimages/academic-conference-primary-132249422941807450.jpg'
+            }}
+            className="w-full h-full object-cover"
+            style={{
+              opacity: isHovered ? 0.7 : 1,
+              transition: 'opacity 0.3s',
+            }}
+          />
+        ) : (
+          <img
+            src={image.url}
+            onError={(e) => {
+              e.target.src =
+                'https://cphfcrflaa.cloudimg.io/_bcuimages/academic-conference-primary-132249422941807450.jpg'
+            }}
+            className="w-full h-full object-cover"
+          />
+        )}
+      </button>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleFileChange}
+        ref={showImagePicker}
+        style={{ display: 'none' }}
       />
     </div>
   )
