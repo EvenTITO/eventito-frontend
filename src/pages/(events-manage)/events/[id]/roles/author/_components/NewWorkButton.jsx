cc10dee2d3a865ef540d6667cd68e3d238179140
Lucas <lveron@prismamp.com>
2024-10-14 01:26:31 -0300

    fix datetime deadline button disable

--- src/pages/(events-manage)/events/[id]/roles/author/_components/NewWorkButton.jsx
+++ src/pages/(events-manage)/events/[id]/roles/author/_components/NewWorkButton.jsx
@@ -1,24 +1,21 @@
 import { Plus } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { useGetEvent } from '@/hooks/events/useEventState'
-import { dateIsValid, getDeadlineSubmissions } from '@/lib/dates'
+import { dateIsValid, getDeadlineSubmissions, timeIsUp } from '@/lib/dates'
 import { useNavigator } from '@/lib/navigation'
 
 export default function NewWorkButton() {
   const navigator = useNavigator()
   const { data: eventData } = useGetEvent()
-  const deadlineSubmissions = getDeadlineSubmissions(eventData)?.date
+  const deadlineDate = getDeadlineSubmissions(eventData)?.date
+  const deadlineTime = getDeadlineSubmissions(eventData)?.time
 
   function handleNewSubmission() {
     navigator.foward('new-work')
   }
 
-  if (!dateIsValid(deadlineSubmissions)) {
-    return null
-  }
-
   return (
-    <Button onClick={handleNewSubmission}>
+    <Button onClick={handleNewSubmission} disabled={timeIsUp(deadlineDate, deadlineTime)}>
       <Plus className="mr-2 h-4 w-4" /> Nueva entrega
     </Button>
   )
