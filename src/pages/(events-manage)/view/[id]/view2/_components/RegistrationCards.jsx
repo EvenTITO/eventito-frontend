96c81e59b9ae7ed9005f592b7b531f24377d769f
gsabatino9 <gsabatino@fi.uba.ar>
2024-12-18 16:00:45 -0300

    view2

---
+++ src/pages/(events-manage)/view/[id]/view2/_components/RegistrationCards.jsx
@@ -0,0 +1,28 @@
+import CardWithFocus from '@/components/Card/CardWithFocus'
+import { format } from '@formkit/tempo'
+
+export default function RegistrationCard({ open, close, isOpen, limitDate }) {
+  return (
+    <CardWithFocus
+      onClick={() => alert('ok')}
+      rightComponent={<LimitDate limitDate={limitDate} isOpen={isOpen} />}
+    >
+      <div className="flex-grow">
+        <h2 className="text-lg font-medium text-foreground">
+          {isOpen ? open.title : close.title}
+        </h2>
+        <p className="text-lg text-muted-foreground italic">
+          {isOpen ? open.description : close.description}
+        </p>
+      </div>
+    </CardWithFocus>
+  )
+}
+
+function LimitDate({ limitDate, isOpen }) {
+  if (isOpen) {
+    return <p className="text-gray-500">Fecha límite: {format(limitDate)}</p>
+  }
+
+  return <p className="text-gray-500">Cerró: {format(limitDate)}</p>
+}
--- src/pages/(events-manage)/view/[id]/view2/page.jsx
+++ src/pages/(events-manage)/view/[id]/view2/page.jsx
@@ -2,8 +2,13 @@ import Banner from './_components/Banner'
 import ContainerPage from '@/pages/(events-manage)/_components/containerPage'
 import MetadataList from './_components/MetadataList'
 import TitleEvent from './_components/TitleEvent'
+import RegistrationCard from './_components/RegistrationCards'
+import { getStartDate } from '@/lib/dates'
+import { getDates } from '@/pages/(events-manage)/manage/[id]/administration/_components/utils'
 
 export default function Page({ eventInfo }) {
+  const [startDate, endDate, submissionLimit] = getDates(eventInfo)
+
   return (
     <div className="space-y-6">
       <Banner title={eventInfo.title} bannerURL={eventInfo.bannerURL} />
@@ -15,6 +20,33 @@ export default function Page({ eventInfo }) {
             contact={eventInfo.contact}
             organizedBy={eventInfo.organized_by}
           />
+
+          <div>
+            <RegistrationCard
+              open={{
+                title: 'Inscripciones abiertas para asistir al evento',
+                description: 'Haz click para ir al formulario de inscripción',
+              }}
+              close={{
+                title: 'Inscripciones cerradas para asistir al evento',
+                description: 'Las inscripciones cerraron ayer',
+              }}
+              isOpen={new Date() < startDate}
+              limitDate={startDate}
+            />
+            <RegistrationCard
+              open={{
+                title: 'Inscripciones abiertas para asistir al evento',
+                description: 'Haz click para ir al formulario de inscripción',
+              }}
+              close={{
+                title: 'Inscripciones cerradas para asistir al evento',
+                description: 'Las inscripciones cerraron ayer',
+              }}
+              limitDate={submissionLimit}
+              isOpen={new Date() < submissionLimit}
+            />
+          </div>
         </div>
       </ContainerPage>
     </div>
