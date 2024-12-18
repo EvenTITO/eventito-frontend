3ce37059449dda2f54d721bff34d4b146d2f2957
gsabatino9 <gsabatino@fi.uba.ar>
2024-12-18 17:44:17 -0300

    view2

--- src/pages/(events-manage)/view/[id]/_components/ContainerViewPage.jsx
+++
@@ -1,5 +0,0 @@
-export default function ContainerViewPage({ children }) {
-  return (
-    <div className="min-h-screen bg-background text-foreground">{children}</div>
-  )
-}
--- src/pages/(events-manage)/view/routes.jsx
+++ src/pages/(events-manage)/view/routes.jsx
@@ -1,22 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import EventViewPage from './[id]/index'
 import LayoutViewEvent from './layout'
-import RegistrationPage from './[id]/registration'
 import ProtectedRoute from './protection'
-import EventViewPage2 from './[id]/view2'
 
 export default function RouteViewEvent() {
   return (
     <Routes>
       <Route path="/view/events" element={<LayoutViewEvent />}>
-        <Route
-          path=":id/2"
-          element={
-            <ProtectedRoute>
-              <EventViewPage2 />
-            </ProtectedRoute>
-          }
-        />
         <Route
           path=":id/"
           element={
@@ -30,14 +20,6 @@ export default function RouteViewEvent() {
           element={
             <ProtectedRoute>
               <EventViewPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path=":id/register"
-          element={
-            <ProtectedRoute>
-              <RegistrationPage />
             </ProtectedRoute>
           }
         />
